a88e7ec73173bac4d9aedbabe127edbec5b02e4b
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-20 09:43:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,13 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
-  temp_array = []
-  sentence_array.each do |word|
-    temp_array.push(word.reverse)
-  end
-  temp_array.join(" ")
+#  sentence_array = sentence.split(" ")
+#  temp_array = []
+#  sentence_array.each do |word|
+#    temp_array.push(word.reverse)
+#  end
+#  temp_array.join(" ")
+
+sentence_array = sentence.split(" ")
+sentence_array.collect { |word|
+  word.reverse
+}
 end
