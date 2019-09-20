d28e3c7958b51eb989f110dff9199b81f8675dff
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-20 09:32:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(sentence)
-  temp_string = ''
+  temp_string = ""
   temp_array = sentence.split(" ")
   temp_array.each { |index|
-    temp_string = temp_string + " " + temp_array[index].reverse
+    not_yet = temp_array[index]
+    temp_string = temp_string + " " + not_yet.reverse
   }
   temp_string
 end
